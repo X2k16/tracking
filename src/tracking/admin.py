244405d0f1681c83e0959cee76fbab419b23907c
chibiegg <chibiegg@chibiegg.net>
2016-01-24 05:19:27 +0900

    端末情報を追加

--- src/tracking/admin.py
+++ src/tracking/admin.py
@@ -1,9 +1,21 @@
 # coding=utf-8
 from django.contrib import admin
-from tracking.models import Participant, AttendLog
+from tracking.models import Participant, AttendLog, Terminal
 
 
 class ParticipantAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("id", "card_id")
 
 admin.site.register(Participant, ParticipantAdmin)
+
+class TerminalAdmin(admin.ModelAdmin):
+    list_display = ("id", "mac", "name", "venue")
+    list_filter = ("venue",)
+
+admin.site.register(Terminal, TerminalAdmin)
+
+
+class AttendLogAdmin(admin.ModelAdmin):
+    list_display = ("id", "terminal", "venue", "program")
+
+admin.site.register(AttendLog, AttendLogAdmin)
--- src/tracking/migrations/0001_initial.py
+++ src/tracking/migrations/0001_initial.py
@@ -10,25 +10,81 @@ class Migration(migrations.Migration):
 
     dependencies = [
         ('auth', '0006_require_contenttypes_0002'),
+        ('program', '0001_initial'),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='Participant',
+            name='AttendLog',
             fields=[
-                ('user_ptr', models.OneToOneField(to=settings.AUTH_USER_MODEL, auto_created=True, parent_link=True, primary_key=True, serialize=False)),
-                ('card_id', models.CharField(max_length=255, verbose_name='カードID')),
-                ('login_token', models.CharField(unique=True, max_length=100, verbose_name='ログイントークン')),
-                ('job_type', models.CharField(blank=True, max_length=100, choices=[('Web', 'Web'), ('通信', '通信'), ('ほげ', 'ほげ'), ('fuga', 'fuga')], verbose_name='職種')),
+                ('id', models.AutoField(serialize=False, verbose_name='ID', auto_created=True, primary_key=True)),
+                ('date', models.DateTimeField(verbose_name='確認日時')),
+                ('created_at', models.DateTimeField(auto_now_add=True, verbose_name='登録日時')),
+                ('updated_at', models.DateTimeField(auto_now=True, verbose_name='最終更新日時')),
             ],
             options={
+                'verbose_name_plural': '入場履歴',
+                'verbose_name': '入場履歴',
+                'ordering': ('date',),
+            },
+        ),
+        migrations.CreateModel(
+            name='Participant',
+            fields=[
+                ('user_ptr', models.OneToOneField(serialize=False, to=settings.AUTH_USER_MODEL, parent_link=True, auto_created=True, primary_key=True)),
+                ('card_id', models.CharField(verbose_name='カードID', max_length=255)),
+                ('login_token', models.CharField(verbose_name='ログイントークン', unique=True, max_length=100)),
+                ('job_type', models.CharField(choices=[('Web', 'Web'), ('通信', '通信'), ('ほげ', 'ほげ'), ('fuga', 'fuga')], max_length=100, verbose_name='職種', blank=True)),
+            ],
+            options={
+                'verbose_name_plural': 'users',
                 'verbose_name': 'user',
                 'abstract': False,
-                'verbose_name_plural': 'users',
             },
             bases=('auth.user',),
             managers=[
                 ('objects', django.contrib.auth.models.UserManager()),
             ],
+        ),
+        migrations.CreateModel(
+            name='Terminal',
+            fields=[
+                ('id', models.AutoField(serialize=False, verbose_name='ID', auto_created=True, primary_key=True)),
+                ('mac', models.CharField(verbose_name='MACアドレス', unique=True, max_length=17)),
+                ('name', models.CharField(max_length=200, verbose_name='名前', blank=True)),
+                ('created_at', models.DateTimeField(auto_now_add=True, verbose_name='登録日時')),
+                ('updated_at', models.DateTimeField(auto_now=True, verbose_name='最終更新日時')),
+                ('venue', models.ForeignKey(null=True, to='program.Venue', blank=True)),
+            ],
+            options={
+                'verbose_name_plural': 'NFC端末',
+                'verbose_name': 'NFC端末',
+                'ordering': ('name', 'id'),
+            },
+        ),
+        migrations.AddField(
+            model_name='attendlog',
+            name='participant',
+            field=models.ForeignKey(to='tracking.Participant'),
+        ),
+        migrations.AddField(
+            model_name='attendlog',
+            name='program',
+            field=models.ForeignKey(null=True, to='program.Program', blank=True),
+        ),
+        migrations.AddField(
+            model_name='attendlog',
+            name='terminal',
+            field=models.ForeignKey(null=True, to='tracking.Terminal', blank=True),
+        ),
+        migrations.AddField(
+            model_name='attendlog',
+            name='timespan',
+            field=models.ForeignKey(null=True, to='program.Timespan', blank=True),
+        ),
+        migrations.AddField(
+            model_name='attendlog',
+            name='venue',
+            field=models.ForeignKey(null=True, to='program.Venue', blank=True),
         ),
     ]
