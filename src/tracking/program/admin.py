e91dabd3a27c12eef9aa63fa8ddf6a58a8555113
chibiegg <egusa@takenoko-tech.co.jp>
2016-01-28 21:05:15 +0900

    今いる会場を保存

--- src/tracking/program/admin.py
+++ src/tracking/program/admin.py
@@ -20,3 +20,17 @@ class ProgramAdmin(admin.ModelAdmin):
     list_filter = ("timespan", "venue")
 
 admin.site.register(Program, ProgramAdmin)
+
+
+class VenueAttendanceAdmin(admin.ModelAdmin):
+    list_display = ("id", "participant", "venue", "is_enabled")
+    list_filter = ("venue", "is_enabled")
+
+admin.site.register(VenueAttendance, VenueAttendanceAdmin)
+
+
+class ProgramAttendanceAdmin(admin.ModelAdmin):
+    list_display = ("id", "participant", "program", "is_enabled")
+    list_filter = ("program__venue", "timespan", "is_enabled")
+
+admin.site.register(ProgramAttendance, ProgramAttendanceAdmin)
